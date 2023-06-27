04c1191a6aba67419e56009760c2f0cb9e05e076
Nolan DE PUYDT <nodepuydt@gmail.com>
2023-06-27 11:36:35 +0200

    Init project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import Home from "./pages/Home";
-
+import "./reset.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <h1>coucou</h1>
     </div>
   );
 }
