eedfb491d4d13767fa659c445dce2514797c0ea4
Nolan DE PUYDT <nodepuydt@gmail.com>
2023-06-29 00:10:48 +0200

    Created navbar. Conditionnal render to be done.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+// Imports CSS
 import "./reset.css";
 import "./App.css";
+
+// Imports components
+import Navbar from "./components/Navbar";
+
+// Imports contexts
+import { MenuContextProvider } from "./contexts/MenuContext";
 
 function App() {
   return (
     <div className="App">
-      <h1>coucou</h1>
+      <MenuContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>{/* <Route path="/" element={<Connection />} /> */}</Routes>
+        </Router>
+      </MenuContextProvider>
     </div>
   );
 }
