7873b27565bd9fc8d091f108c49122e832c2b210
Jasmine Grozinger <jasmine.grozinger@gmail.com>
2023-06-29 16:10:46 +0200

    admin-section: user-management modified

--- frontend/src/pages/AdminUserManagement.jsx
+++ frontend/src/pages/AdminUserManagement.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
 
@@ -9,27 +10,40 @@ import ButtonAddUser from "../components/ButtonAddUser";
 import "./AdminUserManagement.css";
 import ModalEditUser from "../components/ModalEditUser";
 
+import AuthContext from "../contexts/AuthContext";
+
 function AdminUserManagement() {
+  const { isAdmin } = useContext(AuthContext);
+
   const [getUsers, setGetUsers] = useState([]);
+
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_URL}/users`)
-      .then((response) => {
-        setGetUsers(response.data);
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
+    if (isAdmin) {
+      axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/users`)
+        .then((response) => {
+          setGetUsers(response.data);
+        })
+        .catch((err) => {
+          console.error(err.message);
+        });
+    }
   }, []);
   return (
     <div className="mainAdmin">
-      <h1 className="welcome">Welcome</h1>
-      {getUsers.map((el, index) => (
-        <UserList key={getUsers[index]} el={el} />
-      ))}
-      <ButtonAddUser />
+      {isAdmin ? (
+        <div>
+          <h1 className="welcome">Welcome</h1>
+          {getUsers.map((el, index) => (
+            <UserList key={getUsers[index]} el={el} />
+          ))}
+          <ButtonAddUser />
 
-      <ModalEditUser />
+          <ModalEditUser />
+        </div>
+      ) : (
+        useNavigate("/")
+      )}
     </div>
   );
 }
